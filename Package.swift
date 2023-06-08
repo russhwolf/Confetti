690649ee06ca972ba4dda7336edea8db70a45826
runner <runner@Mac-1686246278133.local>
2023-06-08 18:15:00 +0000

    KMM SPM package release for 0.8.1

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://api.github.com/repos/joreilly/Confetti/releases/assets/81812486.zip"
-let remoteKotlinChecksum = "5edc28cfe7ee4c1deb542819725bcf61f6dfbe87b42753c6bea6afbc545019c4"
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/russhwolf/Confetti/Confetti/shared-kmmbridge/0.8.1/shared-kmmbridge-0.8.1.zip"
+let remoteKotlinChecksum = "8648104b4ca2f84a9cd0b0df2134a3ac64ec1f7a5f6a5b8d5e8750b8d75fc710"
 let packageName = "ConfettiKit"
+// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
